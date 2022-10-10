568b073aec9e1662de0bee2954640a4a6f2c8152
Pratyush Pandey <66225351+Pr-att@users.noreply.github.com>
2022-10-11 03:35:51 +0530

    Update

--- main.py
+++ main.py
@@ -20,18 +20,4 @@
 # end = time.time()
 # print(end - start)
 
-numRows = 5
-default = [[1], [1, 1]]
-if numRows == 1:
-    print([[1]])
-elif numRows == 2:
-    print([[1], [1, 1]])
-else:
-    p = 2
-    for i in range(numRows - 2):
-        default.append([1])
-        for j in range(i + 1):
-            default[p].append(sum(default[p - 1][j:j + 2: 1]))
-        default[p].append(1)
-        p += 1
-print(default)
+
