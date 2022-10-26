37d5b60d09164d235eb22bfcbd64de9065184976
Pr-att <pp56355@gmail.com>
2022-10-26 12:52:07 +0530

    Commit

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
-# s1 = "hello"
-# s2 = "ooolleoooleh"
-# myMap1 = {}
-# myMap2 = {}
-# for i in s1:
-#     if i in myMap1:
-#         myMap1[i] += 1
-#     else:
-#         myMap1[i] = 1
-# init, endL = 0, 0
-# count = len(s1)
-# stack = dict([(key, value) for key, value in myMap1.items()])
-# while True:
-#     if count == 0 and init <= len(s2):
-#         print(True)
-#         break
-#     elif init >= len(s2) and count > 0:
-#         print(False)
-#         break
-#     if s2[init] in myMap1 and count > 0 and stack[s2[init]] > 0:
-#         endL = init
-#         stack[s2[init]] -= 1
-#         count -= 1
-#     else:
-#         count = len(s1)
-#         stack = dict([(key, value) for key, value in myMap1.items()])
-#     init += 1
-
-nums = [1, 2, 1]
-k = 1
-myMap = {}
-for i in nums:
-    if i in myMap:
-        myMap[i] += 1
+def factors(num):
+    length = 0
+    myList = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            length += 1
+            myList.append(i)
+    if length == 2:
+        return myList[0]
     else:
-        myMap[i] = 1
+        return myList[length // 2]
 
+count = 0
+def downToZero(n):
+    global count
+    if n == 1:
+        return count + 1
+    temp = factors(n)
+    if  temp == 1:
+        n -= 1
+        count += 1
+        return downToZero(n)
+    else:
+        n = max(temp, n // temp)
+        count += 1
+        return downToZero(n)
+
+print(downToZero(4))
